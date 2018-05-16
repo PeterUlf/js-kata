337100fe6aedb7fb7c1668fe4020633dee7e994c
PeterUlf <peter@puj.dk>
2018-05-16 13:25:27 +0200

    Tirsdagsopgaven

--- w20tuesday/materials/mockify.js
+++ w20tuesday/materials/mockify.js
@@ -2,3 +2,31 @@
 
 // create a function called `mockify` that receives a string, 
 // and returns a mocified version of that string.
+
+
+function mockify(myString) {
+    const letters = Array.from(myString);
+    var arrayLength = letters.length;
+    let myNewStringArray = [];
+    let newLetter = "";
+    for (var i = 0; i < arrayLength; i++) {
+        //alert(letters[i]);
+        //Do something
+
+
+        newLetter = letters[i].toLowerCase();
+
+        if (Math.random() > 0.5) {
+            newLetter = newLetter.toUpperCase();
+        }
+        myNewStringArray.push(newLetter);
+
+
+
+    }
+    console.log(myNewStringArray.join(""));
+
+}
+
+
+console.log(mockify('This is a test'));
