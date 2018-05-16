0f81709998bfc4d9303acf3729c280c3ad509356
PeterUlf <peter@puj.dk>
2018-05-16 12:55:08 +0200

    done

---
+++ w20monday/mysolution/javascript.js
@@ -0,0 +1,9 @@
+document.getElementById("btnFjernCookies").addEventListener("click", fjernCookies);
+
+
+
+
+function fjernCookies() {
+    var element = document.getElementById("cookies");
+    element.classList.add("slidedown");
+}
